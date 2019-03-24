3700c04680b61fa4cf2ffa4e6197e442c2992e8e
Wesly Fernandez <wesly_fernandez1@outlook.com>
2019-03-24 13:17:46 -0600

    UPD: Variable entorno mongo - heroku

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB = '';
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://strider:7nhSYUjJr4ewB8Wb@cluster0-stjvq.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
